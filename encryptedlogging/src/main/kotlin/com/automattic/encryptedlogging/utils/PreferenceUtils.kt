31fdcc45d9e2996c97090021b528e7611b1de7e6
Alex Forcier <alex.forcier@automattic.com>
2018-08-03 11:41:34 -0400

    Add helper function for obtaining FluxC preferences

---
+++ encryptedlogging/src/main/kotlin/com/automattic/encryptedlogging/utils/PreferenceUtils.kt
@@ -0,0 +1,11 @@
+package com.automattic.encryptedlogging.utils
+
+import android.content.Context
+import android.content.SharedPreferences
+
+object PreferenceUtils {
+    @JvmStatic
+    fun getFluxCPreferences(context: Context): SharedPreferences {
+        return context.getSharedPreferences("${context.packageName}_fluxc-preferences", Context.MODE_PRIVATE)
+    }
+}
