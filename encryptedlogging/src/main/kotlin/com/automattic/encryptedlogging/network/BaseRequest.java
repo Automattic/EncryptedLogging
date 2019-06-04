1c1fc20b6b320d0c0242a284a2b59b1a432a027c
vojtasmrcek <planarvoid@gmail.com>
2019-06-04 10:32:10 +0200

    Reorder imports with the new rule

--- encryptedlogging/src/main/kotlin/com/automattic/encryptedlogging/network/BaseRequest.java
+++ encryptedlogging/src/main/kotlin/com/automattic/encryptedlogging/network/BaseRequest.java
@@ -2,8 +2,9 @@ package com.automattic.encryptedlogging.network;
 
 import android.net.Uri;
 import android.net.Uri.Builder;
-import androidx.annotation.NonNull;
 import android.util.Base64;
+
+import androidx.annotation.NonNull;
 
 import com.android.volley.AuthFailureError;
 import com.android.volley.Cache;
