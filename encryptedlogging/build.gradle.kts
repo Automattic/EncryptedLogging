210cfd96976174a6fa52c9c11f4ce54e00180ad3
Wojtek Zieba <wojtek.zieba@automattic.com>
2024-04-15 19:34:54 +0200

    Add necessary dependencies

--- encryptedlogging/build.gradle.kts
+++ encryptedlogging/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.androidLibrary)
     alias(libs.plugins.jetbrainsKotlinAndroid)
+    alias(libs.plugins.jetbrainsKotlinKapt)
 }
 
 android {
@@ -32,10 +33,21 @@ android {
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
+    implementation(libs.eventbus)
+    implementation(libs.fluxc.annotations)
+    implementation(libs.jna)
+    implementation(libs.kotlin.coroutines)
+    implementation(libs.lazysodium.android)
+    implementation(libs.volley)
+    implementation(libs.wordpress.utils)
+    implementation(libs.wordpress.wellsql)
+    kapt(libs.fluxc.processor)
+    kapt(libs.wordpress.wellsql.processor)
+    testImplementation(libs.assertj)
     testImplementation(libs.junit)
+    testImplementation(libs.robolectric)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -16,6 +16,15 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
+        maven(url = "https://a8c-libs.s3.amazonaws.com/android") {
+            content {
+                includeGroup("org.wordpress")
+                includeGroup("org.wordpress.wellsql")
+                includeGroup("org.wordpress.wellsql.wellsql-processor")
+                includeGroup("org.wordpress.fluxc")
+                includeGroup("org.wordpress.fluxc.plugins")
+            }
+        }
     }
 }
 
