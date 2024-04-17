7b08ee10ba469c1e7e7f029190863f506e2b301d
Wojtek Zieba <wojtek.zieba@automattic.com>
2024-04-17 15:55:24 +0200

    Migrate `Dispatcher` to Kotlin

--- encryptedlogging/src/main/kotlin/com/automattic/encryptedlogging/Dispatcher.kt
+++ encryptedlogging/src/main/kotlin/com/automattic/encryptedlogging/Dispatcher.kt
@@ -1,45 +1,41 @@
-package com.automattic.encryptedlogging;
+package com.automattic.encryptedlogging
 
-import org.greenrobot.eventbus.EventBus;
-import com.automattic.encryptedlogging.store.Store;
+import com.automattic.encryptedlogging.store.Store
+import org.greenrobot.eventbus.EventBus
+import org.wordpress.android.fluxc.annotations.action.Action
+import org.wordpress.android.util.AppLog
 
-import org.wordpress.android.fluxc.annotations.action.Action;
-import org.wordpress.android.util.AppLog;
-import org.wordpress.android.util.AppLog.T;
+internal class Dispatcher {
+    private val mBus: EventBus = EventBus.builder()
+        .logNoSubscriberMessages(true)
+        .sendNoSubscriberEvent(true)
+        .throwSubscriberException(true)
+        .build()
 
-public class Dispatcher {
-    private final EventBus mBus;
-
-    public Dispatcher() {
-        mBus = EventBus.builder()
-                .logNoSubscriberMessages(true)
-                .sendNoSubscriberEvent(true)
-                .throwSubscriberException(true)
-                .build();
-    }
-
-    public void register(final Object object) {
-        mBus.register(object);
-        if (object instanceof Store) {
-            ((Store) object).onRegister();
+    fun register(`object`: Any?) {
+        mBus.register(`object`)
+        if (`object` is Store) {
+            `object`.onRegister()
         }
     }
 
-    public void unregister(final Object object) {
-        mBus.unregister(object);
+    fun unregister(`object`: Any?) {
+        mBus.unregister(`object`)
     }
 
-    public void dispatch(Action action) {
-        AppLog.d(T.API, "Dispatching action: " + action.getType().getClass().getSimpleName()
-                + "-" + action.getType().toString());
-        post(action);
+    fun dispatch(action: Action<*>) {
+        AppLog.d(
+            AppLog.T.API, "Dispatching action: " + action.type.javaClass.simpleName
+                    + "-" + action.type.toString()
+        )
+        post(action)
     }
 
-    public void emitChange(final Object changeEvent) {
-        mBus.post(changeEvent);
+    fun emitChange(changeEvent: Any?) {
+        mBus.post(changeEvent)
     }
 
-    private void post(final Object event) {
-        mBus.post(event);
+    private fun post(event: Any) {
+        mBus.post(event)
     }
 }
