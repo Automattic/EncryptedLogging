5791894102fc66a8f7c9cca777485646f0e97c5d
Petros Paraskevopoulos <petros.paraskevopoulos@automattic.com>
2025-06-12 14:57:12 +0300

    Kotlin: Convert date time utils to kotlin

--- encryptedlogging/src/main/kotlin/com/automattic/encryptedlogging/utils/DateTimeUtils.kt
+++ encryptedlogging/src/main/kotlin/com/automattic/encryptedlogging/utils/DateTimeUtils.kt
@@ -1,53 +1,55 @@
-package com.automattic.encryptedlogging.utils;
+package com.automattic.encryptedlogging.utils
 
-import java.text.DateFormat;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Locale;
-import java.util.TimeZone;
+import java.text.DateFormat
+import java.text.ParseException
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
+import java.util.TimeZone
 
-public class DateTimeUtils {
-    private DateTimeUtils() {
-        throw new AssertionError();
+public class DateTimeUtils private constructor() {
+    init {
+        throw AssertionError()
     }
 
-    // See http://drdobbs.com/java/184405382
-    private static final ThreadLocal<DateFormat> ISO8601_FORMAT = new ThreadLocal<DateFormat>() {
-        @Override
-        protected DateFormat initialValue() {
-            return new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ", Locale.US);
+    public companion object {
+        // See http://drdobbs.com/java/184405382
+        private val ISO8601_FORMAT: ThreadLocal<DateFormat> = object : ThreadLocal<DateFormat>() {
+            override fun initialValue(): DateFormat {
+                return SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ", Locale.US)
+            }
         }
-    };
 
-    /**
-     * Given an ISO 8601-formatted date as a String, returns a {@link Date} in UTC.
-     */
-    public static Date dateUTCFromIso8601(String iso8601date) {
-        try {
-            iso8601date = iso8601date.replace("Z", "+0000").replace("+00:00", "+0000");
-            DateFormat formatter = ISO8601_FORMAT.get();
-            formatter.setTimeZone(TimeZone.getTimeZone("UTC"));
-            return formatter.parse(iso8601date);
-        } catch (ParseException e) {
-            return null;
+        /**
+         * Given an ISO 8601-formatted date as a String, returns a [Date] in UTC.
+         */
+        public fun dateUTCFromIso8601(iso8601date: String): Date? {
+            var iso8601date = iso8601date
+            try {
+                iso8601date = iso8601date.replace("Z", "+0000").replace("+00:00", "+0000")
+                val formatter = ISO8601_FORMAT.get()
+                formatter!!.timeZone = TimeZone.getTimeZone("UTC")
+                return formatter.parse(iso8601date)
+            } catch (e: ParseException) {
+                return null
+            }
         }
-    }
 
-    /**
-     * Given a {@link Date}, returns an ISO 8601-formatted String in UTC.
-     */
-    public static String iso8601UTCFromDate(Date date) {
-        if (date == null) {
-            return "";
+        /**
+         * Given a [Date], returns an ISO 8601-formatted String in UTC.
+         */
+        public fun iso8601UTCFromDate(date: Date?): String {
+            if (date == null) {
+                return ""
+            }
+            val tz = TimeZone.getTimeZone("UTC")
+            val formatter = ISO8601_FORMAT.get()
+            formatter!!.timeZone = tz
+
+            val iso8601date = formatter.format(date)
+
+            // Use "+00:00" notation rather than "+0000" to be consistent with the WP.COM API
+            return iso8601date.replace("+0000", "+00:00")
         }
-        TimeZone tz = TimeZone.getTimeZone("UTC");
-        DateFormat formatter = ISO8601_FORMAT.get();
-        formatter.setTimeZone(tz);
-
-        String iso8601date = formatter.format(date);
-
-        // Use "+00:00" notation rather than "+0000" to be consistent with the WP.COM API
-        return iso8601date.replace("+0000", "+00:00");
     }
 }
