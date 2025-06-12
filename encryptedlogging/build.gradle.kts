8e45dd04134fede9487e3e1544a00dfe10754020
Petros Paraskevopoulos <petros.paraskevopoulos@automattic.com>
2025-06-12 14:22:41 +0300

    Build: Use java 11 for compile source/target compatibility kotlin target
    FYI: Java 17 could be used instead but most projects are still using 11
    (expect for PCAndroid).

--- encryptedlogging/build.gradle.kts
+++ encryptedlogging/build.gradle.kts
@@ -30,11 +30,11 @@ android {
     }
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
     }
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_1_8.toString()
+        jvmTarget = JavaVersion.VERSION_11.toString()
     }
     sourceSets["main"].java.srcDirs("src/main/kotlin")
 }
