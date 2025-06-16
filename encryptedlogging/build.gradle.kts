0b52688e6ff155e30555d246fb04b7d51e402ef1
Petros Paraskevopoulos <petros.paraskevopoulos@automattic.com>
2025-06-16 12:43:26 +0300

    Build: Remove unnecessary kotlin source sets configuration
    PR Discussion: https://github.com/Automattic/EncryptedLogging/pull/18
    #discussion_r2149437349

--- encryptedlogging/build.gradle.kts
+++ encryptedlogging/build.gradle.kts
@@ -36,9 +36,6 @@ android {
     kotlinOptions {
         jvmTarget = JavaVersion.VERSION_11.toString()
     }
-    sourceSets["main"].java.srcDirs("src/main/kotlin")
-    sourceSets["test"].java.srcDirs("src/test/kotlin")
-    sourceSets["androidTest"].java.srcDirs("src/androidTest/kotlin")
 }
 
 kotlin {
