1be9273c14f48c30c2ed0761ba986a0cc141f5dd
Wojtek Zieba <wojtek.zieba@automattic.com>
2025-06-25 11:22:11 +0300

    Migration: Introduce type converter for upload state
    h/t to @wzieba - PR Comment: https://github.com/Automattic/
    EncryptedLogging/pull/20#pullrequestreview-2957158525

--- encryptedlogging/src/main/kotlin/com/automattic/encryptedlogging/persistence/dao/EncryptedLogDao.kt
+++ encryptedlogging/src/main/kotlin/com/automattic/encryptedlogging/persistence/dao/EncryptedLogDao.kt
@@ -6,28 +6,29 @@ import androidx.room.Insert
 import androidx.room.Query
 import androidx.room.Upsert
 import com.automattic.encryptedlogging.model.encryptedlogging.EncryptedLogEntity
+import com.automattic.encryptedlogging.model.encryptedlogging.EncryptedLogUploadState
 
 @Dao
 internal abstract class EncryptedLogDao {
     @Query("""
         SELECT * FROM EncryptedLogEntity 
-        WHERE uploadStateDbValue IN (:uploadStates) 
-        ORDER BY uploadStateDbValue ASC, id ASC 
+        WHERE uploadState IN (:uploadStates) 
+        ORDER BY uploadState ASC, id ASC 
         LIMIT 1
     """)
-    internal abstract suspend fun getEncryptedLog(uploadStates: List<Int>): EncryptedLogEntity?
+    internal abstract suspend fun getEncryptedLog(vararg uploadStates: EncryptedLogUploadState): EncryptedLogEntity?
 
     @Query("""
         SELECT * FROM EncryptedLogEntity 
-        WHERE uploadStateDbValue = :uploadState
+        WHERE uploadState = :uploadState
     """)
-    internal abstract suspend fun getEncryptedLogs(uploadState: Int): List<EncryptedLogEntity>
+    internal abstract suspend fun getEncryptedLogs(uploadState: EncryptedLogUploadState): List<EncryptedLogEntity>
 
     @Query("""
         SELECT COUNT(*) FROM EncryptedLogEntity 
-        WHERE uploadStateDbValue = :uploadState
+        WHERE uploadState = :uploadState
     """)
-    internal abstract suspend fun getEncryptedLogsCount(uploadState: Int): Int
+    internal abstract suspend fun getEncryptedLogsCount(uploadState: EncryptedLogUploadState): Int
 
     @Insert
     internal abstract suspend fun insertEncryptedLog(encryptedLog: EncryptedLogEntity)
