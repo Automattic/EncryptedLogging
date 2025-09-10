6990d7c3b2839ac7ecc2e536d557b1c1e869b995
Petros Paraskevopoulos <petros.paraskevopoulos@automattic.com>
2025-09-10 14:49:22 +0300

    Fix: Add logs size limit and pre-trip logs before encrypting
    FYI: This 5MB max logs size limit is both arbitrary and empirical
    because it seems that for (at least) some standard devices, large logs
    files of 100MB+ can easily cause OOM. Also, this 5MB max logs size limit
    is chosen because the API accepts logs up to 10MB, but we leave enough
    headroom for encoding overhead.
    
    PS: It is worth to note that we could apply a different kind of a
    solution (better one), like streaming encryption or chunked processing.
    But, I don't think it is worth it, nor I think it is worth sending such
    large log files to the API.

--- encryptedlogging/src/main/kotlin/com/automattic/encryptedlogging/model/encryptedlogging/LogEncrypter.kt
+++ encryptedlogging/src/main/kotlin/com/automattic/encryptedlogging/model/encryptedlogging/LogEncrypter.kt
@@ -1,12 +1,16 @@
 package com.automattic.encryptedlogging.model.encryptedlogging
 
 import android.util.Base64
+import android.util.Log
 import com.goterl.lazysodium.interfaces.SecretStream
 import com.goterl.lazysodium.interfaces.SecretStream.State
 import com.goterl.lazysodium.utils.Key
+import java.util.Locale
 
 private const val ENCODED_ENCRYPTED_KEY_LENGTH = 108
 private const val ENCODED_HEADER_LENGTH = 32
+// The API accepts logs up to 10MB, but we leave enough headroom for encoding overhead
+private const val MAX_LOG_SIZE = 5 * 1024 * 1024 // 5MB
 
 internal data class EncryptedLoggingKey(val publicKey: Key)
 
@@ -17,27 +21,48 @@ internal data class EncryptedLoggingKey(val publicKey: Key)
  *
  */
 internal class LogEncrypter(private val encryptedLoggingKey: EncryptedLoggingKey) {
+    companion object {
+        private val TAG = LogEncrypter::class.java.simpleName
+    }
+
     /**
      * Encrypts the given [text]. It also adds the given [uuid] to its headers.
      *
      * @param text Text contents to be encrypted
      * @param uuid Uuid for the encrypted log
      */
-    fun encrypt(text: String, uuid: String): String = buildString {
-        val state = State.ByReference()
-        append(buildHeader(uuid, state))
-        val lines = text.lines()
-        lines.asSequence().mapIndexed { index, line ->
-            if (index + 1 >= lines.size) {
-                // If it's the last element
-                line
-            } else {
-                "$line\n"
-            }
-        }.forEach { line ->
-            append(buildMessage(line, state))
+    fun encrypt(text: String, uuid: String): String {
+        val trimmedText = if (text.length > MAX_LOG_SIZE) {
+            Log.w(TAG, "Log with uuid $uuid is too big (${text.length.toMB()}mb), " +
+                    "max allowed log size is ${MAX_LOG_SIZE.toMB()}mb; log got trimmed.")
+            text.takeLast(MAX_LOG_SIZE) // Keep the last N bytes (most recent logs)
+        } else {
+            text
         }
-        append(buildFooter(state))
+        return buildString {
+            val state = State.ByReference()
+            append(buildHeader(uuid, state))
+            val lines = trimmedText.lines()
+            lines.asSequence().mapIndexed { index, line ->
+                if (index + 1 >= lines.size) {
+                    // If it's the last element
+                    line
+                } else {
+                    "$line\n"
+                }
+            }.forEach { line ->
+                append(buildMessage(line, state))
+            }
+            append(buildFooter(state))
+        }
+    }
+
+    private fun Int.toMB(): String {
+        return String.format(
+            Locale.US,
+            "%.2f",
+            this.toFloat() / 1024.0 / 1024.0
+        )
     }
 
     /**
