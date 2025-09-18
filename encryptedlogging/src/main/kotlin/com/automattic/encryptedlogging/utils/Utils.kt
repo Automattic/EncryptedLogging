ae25826f9152c580be9429a5546dbc065fa41f30
Petros Paraskevopoulos <9729923+ParaskP7@users.noreply.github.com>
2025-09-18 11:48:32 +0300

    Refactor: Remove unnecessary internal keyword on utils functions
    PR Comment: https://github.com/Automattic/EncryptedLogging/pull/28
    #discussion_r2355022277
    
    Co-authored-by: Wojciech Zięba <wojtek.zieba@automattic.com>

--- encryptedlogging/src/main/kotlin/com/automattic/encryptedlogging/utils/Utils.kt
+++ encryptedlogging/src/main/kotlin/com/automattic/encryptedlogging/utils/Utils.kt
@@ -3,13 +3,13 @@ package com.automattic.encryptedlogging.utils
 import java.util.Locale
 
 internal object Utils {
-    internal fun Int.toMB() = String.format(
+    fun Int.toMB() = String.format(
         Locale.US,
         "%.2f",
         this.toFloat() / 1024.0 / 1024.0
     )
 
-    internal fun Long.toMB() = String.format(
+    fun Long.toMB() = String.format(
         Locale.US,
         "%.2f",
         this.toFloat() / 1024.0 / 1024.0
